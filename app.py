5f1820f0b4e62b2ad002b3c7483516d6bca891d3
pos1er <Dima985189@ya.ru>
2023-01-04 20:45:17 +0300

    start test

--- app.py
+++ app.py
@@ -1,36 +1,70 @@
+from utils.notify_admins import on_startup_notify
 from loader import bot
 # from handlers.users.timers import CheckTime
 from utils.default_commands import set_default_commands
 from handlers.users import start_menu, admin_panel
 from middlewares.throttling import ThrottlingMiddleware, BanAcceptCheck, LanguageCheck
+from aiohttp.web import run_app
+from aiohttp.web_app import Application
+
+from aiogram.webhook.aiohttp_server import SimpleRequestHandler, setup_application
+from aiogram import Router
 import asyncio
 import logging
 
 logger = logging.getLogger(__name__)
 
+WEBHOOK_URL = f"https://pos1er.com/AAAA"
 
-async def main():
+# You have to guarantee that requests to WEBHOOK_URL are handled by aiohttp webserver.
+# More information about Webhook: https://core.telegram.org/bots/webhooks
+# Webserver settings
+
+
+main_router = Router()
+
+
+@main_router.startup()
+async def on_startup(bot, webhook_url: str):
+    await on_startup_notify(bot)
+    await set_default_commands(bot)
+    await bot.set_webhook(webhook_url)
+
+
+@main_router.shutdown()
+async def on_shutdown(bot):
+    logging.warning("Shutting down..")
+
+    # Insert code here to run it before shutdown
+
+    # Remove webhook (not acceptable in some cases)
+    await bot.delete_webhook()
+
+    logging.warning("Bye!")
+
+
+def main():
     from handlers import dp
-    try:
-        # dp.loop.create_task(CheckTime(10).start_all())
-        from utils.notify_admins import on_startup_notify
-        await on_startup_notify(bot)
-        await set_default_commands(bot)
-        dp.include_router(start_menu.router)
-        dp.include_router(admin_panel.router)
-        dp.update.outer_middleware(BanAcceptCheck())
-        dp.update.outer_middleware(ThrottlingMiddleware())
-        dp.update.outer_middleware(LanguageCheck())
+    dp["webhook_url"] = WEBHOOK_URL
+    dp.include_router(main_router)
 
-        await dp.start_polling(bot, dispatcher=dp, allowed_updates=dp.resolve_used_update_types())
-    finally:
-        await dp.storage.close()
-        # await dp.storage.wait_closed()
-        await bot.session.close()
+    dp.include_router(start_menu.router)
+    dp.include_router(admin_panel.router)
+    dp.update.outer_middleware(BanAcceptCheck())
+    dp.update.outer_middleware(ThrottlingMiddleware())
+    dp.update.outer_middleware(LanguageCheck())
+    
+    app = Application()
+
+    SimpleRequestHandler(
+        dispatcher=dp,
+        bot=bot,
+    ).register(app, path="/AAAA")
+    setup_application(app, dp, bot=bot)
+
+    run_app(app, host="127.0.0.1", port=7771)
 
 
-if __name__ == '__main__':
-    try:
-        asyncio.run(main())
-    except (KeyboardInterrupt, SystemExit):
-        logger.error("Bot stopped!")
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    main()
