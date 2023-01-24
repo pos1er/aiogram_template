48090c0416862a3acf07e95112dd26818925729a
pos1er <Dima985189@ya.ru>
2023-01-24 20:24:30 +0300

    trying

--- app.py
+++ app.py
@@ -1,67 +1,92 @@
-from utils.notify_admins import on_startup_notify
-from loader import bot
-# from handlers.users.timers import CheckTime
-from utils.default_commands import set_default_commands
-from handlers.users import start_menu, admin_panel
-from middlewares.throttling import ThrottlingMiddleware, BanAcceptCheck, LanguageCheck
-from aiohttp.web import run_app
-from aiohttp.web_app import Application
+from os import getenv
+from typing import Any, Dict, Union
 
-from aiogram.webhook.aiohttp_server import SimpleRequestHandler, setup_application
-from aiogram import Router, F, types
-import asyncio
-import logging
+from aiohttp import web
+from handlers.users.start_menu import start_router
 
-logger = logging.getLogger(__name__)
-
-WEBHOOK_URL = f"https://pos1er.com/AAAA"
+from aiogram import Bot, Dispatcher, F, Router
+from aiogram.client.session.aiohttp import AiohttpSession
+from aiogram.exceptions import TelegramUnauthorizedError
+from aiogram.filters import Command, CommandObject
+from aiogram.fsm.storage.redis import DefaultKeyBuilder, RedisStorage
+from aiogram.types import Message
+from aiogram.utils.token import TokenValidationError, validate_token
+from aiogram.webhook.aiohttp_server import (
+    SimpleRequestHandler,
+    TokenBasedRequestHandler,
+    setup_application,
+)
+from data.config import TOKEN
 
 main_router = Router()
 
+BASE_URL = getenv("BASE_URL", "https://pos1er.com")
 
-@main_router.message(F.text)
-async def echo(message: types.Message):
-    # Regular request, add bot: Bot to handler kwargs
-    # await bot.send_message(message.chat.id, message.text)
+WEB_SERVER_HOST = "127.0.0.1"
+WEB_SERVER_PORT = 7771
+MAIN_BOT_PATH = "/AAAA"
+# OTHER_BOTS_PATH = "/webhook/bot/{bot_token}"
+REDIS_DSN = "redis://localhost:6379/0"
 
-    # or reply INTO webhook
-    return bot.send_message(chat_id=message.chat.id, text=message.text)
-
-
-@main_router.startup()
-async def on_startup(bot, webhook_url: str):
-    await on_startup_notify(bot)
-    await set_default_commands(bot)
-    await bot.set_webhook(webhook_url)
-    print(await bot.get_webhook_info())
+# OTHER_BOTS_URL = f"{BASE_URL}{OTHER_BOTS_PATH}"
 
 
-@main_router.shutdown()
-async def on_shutdown(bot):
-    logging.warning("Shutting down..")
-    await bot.delete_webhook()
-    logging.warning("Bye!")
+def is_bot_token(value: str) -> Union[bool, Dict[str, Any]]:
+    try:
+        validate_token(value)
+    except TokenValidationError:
+        return False
+    return True
+
+
+@main_router.message(Command(commands=["add"], magic=F.args.func(is_bot_token)))
+async def command_add_bot(message: Message, command: CommandObject, bot: Bot) -> Any:
+    new_bot = Bot(token=command.args, session=bot.session)
+    try:
+        bot_user = await new_bot.get_me()
+    except TelegramUnauthorizedError:
+        return message.answer("Invalid token")
+    await new_bot.delete_webhook(drop_pending_updates=True)
+    # await new_bot.set_webhook(OTHER_BOTS_URL.format(bot_token=command.args))
+    return await message.answer(f"Bot @{bot_user.username} successful added")
+
+
+@main_router.message(Command(commands=["start"]))
+async def start_command(message: Message, command: CommandObject, bot: Bot) -> Any:
+    return await message.answer(f"Bot")
+
+
+async def on_startup(dispatcher: Dispatcher, bot: Bot):
+    await bot.set_webhook(f"{BASE_URL}{MAIN_BOT_PATH}")
 
 
 def main():
-    from handlers import dp
-    dp["webhook_url"] = WEBHOOK_URL
-    dp.include_router(main_router)
-    dp.include_router(start_menu.router)
-    dp.include_router(admin_panel.router)
-    dp.update.outer_middleware(BanAcceptCheck())
-    dp.update.outer_middleware(ThrottlingMiddleware())
-    dp.update.outer_middleware(LanguageCheck())
-    
-    app = Application()
-    SimpleRequestHandler(
-        dispatcher=dp,
-        bot=bot,
-    ).register(app, path="/AAAA")
-    setup_application(app, dp, bot=bot)
-    run_app(app, host="127.0.0.1", port=7771)
+    session = AiohttpSession()
+    bot_settings = {"session": session, "parse_mode": "HTML"}
+    bot = Bot(token=TOKEN, **bot_settings)
+    storage = RedisStorage.from_url(
+        REDIS_DSN, key_builder=DefaultKeyBuilder(with_bot_id=True))
+
+    main_dispatcher = Dispatcher(storage=storage)
+    main_dispatcher.include_router(main_router)
+    main_dispatcher.startup.register(on_startup)
+
+    multibot_dispatcher = Dispatcher(storage=storage)
+    multibot_dispatcher.include_router(start_router)
+
+    app = web.Application()
+    SimpleRequestHandler(dispatcher=main_dispatcher,
+                         bot=bot).register(app, path=MAIN_BOT_PATH)
+    # TokenBasedRequestHandler(
+    #     dispatcher=multibot_dispatcher,
+    #     bot_settings=bot_settings,
+    # ).register(app, path=OTHER_BOTS_PATH)
+
+    setup_application(app, main_dispatcher, bot=bot)
+    setup_application(app, multibot_dispatcher)
+
+    web.run_app(app, host=WEB_SERVER_HOST, port=WEB_SERVER_PORT)
 
 
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO)
     main()
---
+++ app_last.py
@@ -0,0 +1,66 @@
+from utils.notify_admins import on_startup_notify
+from loader import bot
+# from handlers.users.timers import CheckTime
+from utils.default_commands import set_default_commands
+from handlers.users import start_menu, admin_panel
+from middlewares.throttling import ThrottlingMiddleware, BanAcceptCheck, LanguageCheck
+from aiohttp import web
+
+from aiogram.webhook.aiohttp_server import SimpleRequestHandler, setup_application
+from aiogram import Router, F, types
+import asyncio
+import logging
+
+logger = logging.getLogger(__name__)
+
+WEBHOOK_URL = f"https://pos1er.com/AAAA"
+
+main_router = Router()
+
+
+@main_router.message(F.text)
+async def echo(message: types.Message):
+    # Regular request, add bot: Bot to handler kwargs
+    # await bot.send_message(message.chat.id, message.text)
+
+    # or reply INTO webhook
+    return bot.send_message(chat_id=message.chat.id, text=message.text)
+
+
+@main_router.startup()
+async def on_startup(bot, webhook_url: str):
+    await on_startup_notify(bot)
+    await set_default_commands(bot)
+    await bot.set_webhook(webhook_url)
+    print(await bot.get_webhook_info())
+
+
+@main_router.shutdown()
+async def on_shutdown(bot):
+    logging.warning("Shutting down..")
+    await bot.delete_webhook()
+    logging.warning("Bye!")
+
+
+def main():
+    from handlers import dp
+    dp["webhook_url"] = WEBHOOK_URL
+    dp.include_router(main_router)
+    dp.include_router(start_menu.start_router)
+    dp.include_router(admin_panel.router)
+    dp.update.outer_middleware(BanAcceptCheck())
+    dp.update.outer_middleware(ThrottlingMiddleware())
+    dp.update.outer_middleware(LanguageCheck())
+    
+    app = web.Application()
+    SimpleRequestHandler(
+        dispatcher=dp,
+        bot=bot,
+    ).register(app, path="/AAAA")
+    setup_application(app, dp, bot=bot)
+    web.run_app(app, host="127.0.0.1", port=7771)
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    main()
