1c84ac487c9cd6ec7fe0197ebc0a5f19f976b02e
pos1er <Dima985189@ya.ru>
2023-01-26 23:00:21 +0300

    111

--- captcha/data_structures/callback_data.py
+++
@@ -1,8 +0,0 @@
-from aiogram.dispatcher.filters.callback_data import CallbackData
-
-
-class CaptchaAnswerCallbackData(CallbackData, prefix="captcha"):
-    chat_id: int
-    user_id: int
-    salt: str
-    answer: str
