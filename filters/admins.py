ff39e228ef3a7481afa1a746627a8f58b0a59c49
pos1er <Dima985189@ya.ru>
2023-01-27 18:34:26 +0300

    admin fix2

--- filters/admins.py
+++ filters/admins.py
@@ -7,9 +7,10 @@ import time
 class AdminFilter(BaseFilter):
     admin_right: str = ''
 
-    # async def __call__(self, message: Message):
-    #     if not self.admin_right:
-    #         self.admin_right = ''
+    async def __call__(self, message: Message):
+        # if not self.admin_right:
+        #     self.admin_right = ''
+        return await self.check(message)
 
     async def check(self, message: Message) -> bool:
         return await self.check_admin(message, self.admin_right)
