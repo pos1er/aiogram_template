3762c2894e4160e27a25f4191445ee7ff2360a98
pos1er <Dima985189@ya.ru>
2023-02-05 21:40:24 +0300

    aaaaaaaaaaaaaa

--- bot/handlers/admins/apscheduler.py
+++ bot/handlers/admins/apscheduler.py
@@ -4,7 +4,7 @@ from bot.utils.loggers import app_logger
 
 
 async def send_message_time(bot: Bot):
-    admins_list = MainGets().get_admins_list('notifications.after_start_bot')
+    admins_list = await MainGets().get_admins_list('notifications.after_start_bot')
     app_logger.warning(admins_list)
     for admin in admins_list:
         await bot.send_message(admin['id'], text='Это сообщение отправлено через несколько секунд после старта бота')
@@ -21,13 +21,13 @@ Interval bot message
 
 
 async def send_message_interval(bot: Bot):
-    admins_list = MainGets().get_admins_list('notifications.interval_bot')
+    admins_list = await MainGets().get_admins_list('notifications.interval_bot')
     app_logger.warning(admins_list)
     for admin in admins_list:
         await bot.send_message(admin['id'], text='Это сообщение будет отпавляться с интервалом в 1 минуту')
 
 async def daily_message(bot: Bot):
-    admins_list = MainGets().get_admins_list('notifications.daily')
+    admins_list = await MainGets().get_admins_list('notifications.daily')
     app_logger.warning(admins_list)
     for admin in admins_list:
         await bot.send_message(admin['id'], text='Это сообщение будет отправляться ежедневно в указанное время')
