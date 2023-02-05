1db8509415337f0fd4346397fdadee7f09228f1e
pos1er <Dima985189@ya.ru>
2023-02-05 21:00:44 +0300

    redis scheduler

--- bot/loader.py
+++ bot/loader.py
@@ -3,10 +3,18 @@ from aiogram import Bot, Dispatcher, types
 from aiogram.fsm.storage.redis import RedisStorage
 from bot.data.config import MAIN_TOKEN, REDIS_URL
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from apscheduler.jobstores.redis import RedisJobStore
 
 bot = Bot(token=MAIN_TOKEN, parse_mode='HTML')
 storage = RedisStorage.from_url(REDIS_URL)
-scheduler = AsyncIOScheduler(timezone='Europe/Moscow')
+jobstores = {
+    'default': RedisJobStore(jobs_key='dispatcher_trips_jobs',
+                             run_times_key='dispatcher_trips_running',
+                             host='localhost',
+                             db=2,
+                             port=6379)
+}
+scheduler = AsyncIOScheduler(timezone='Europe/Moscow', jobstores=jobstores)
 #  storage = MemoryStorage()
 # loop = asyncio.get_event_loop()
 dp = Dispatcher(storage=storage)
