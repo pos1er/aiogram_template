b351ac8948aa40398666574e465c48e22718aafd
pos1er <Dima985189@ya.ru>
2023-02-05 21:29:37 +0300

    ghgjhgj

--- bot/loader.py
+++ bot/loader.py
@@ -15,7 +15,7 @@ jobstores = {
                              db=2,
                              port=6379)
 }
-scheduler = AsyncIOScheduler()
+scheduler = AsyncIOScheduler(timezone='Etc/UTC')
 # scheduler = ContextSchedulerDecorator(AsyncIOScheduler(jobstores=jobstores))
 # scheduler.ctx.add_instance(bot, declared_class=Bot)
 #  storage = MemoryStorage()
