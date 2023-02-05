e4e6f6f0a517c57233fdf684f6b8f0ea53ea3572
pos1er <Dima985189@ya.ru>
2023-02-05 20:09:44 +0300

    fix

--- bot/middlewares/i18n.py
+++ bot/middlewares/i18n.py
@@ -12,8 +12,6 @@ from bot.utils.loggers import app_logger
 
 class MyI18nMiddleware(I18nMiddleware):
     async def get_locale(self, event: TelegramObject, data: Dict[str, Any]) -> str:
-        app_logger.warning(data)
-        app_logger.warning(event)
         user_language = await ForFilters().get_language()
         if not user_language:
             user_language = 'en'
